811dd4e20b56654a10c66d8bc5cfc2876ab52ddb
carlincb <carlin.colleen@gmail.com>
2022-01-30 21:28:23 -0700

    Fixed result.value error.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,8 +13,7 @@ const initdb = async () =>
   });
 
 // Logic for a method that accepts some content and adds it to the database
-export const putDb = async (content) => { console.error('putDb not implemented');
-
+export const putDb = async (content) => { 
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
@@ -24,14 +23,13 @@ export const putDb = async (content) => { console.error('putDb not implemented')
 };
 // Logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error('getDb not implemented');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result?.value;
 };
 
 initdb();
